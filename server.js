722eacdfb0958a78b3004cce142ff3e4730e4d81
Lilly.Janssen <Lilly.Janssen@VM19.menke.intranet>
2018-06-25 19:51:26 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+//app.use um middleware zu nutzen
+//next: 
+app.use((req,res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if (err) {
+            console.log("Log nicht beschreibbar");
+        }
+    });
+    next();
+});
+/*
+app.use((req,res,next) => {
+    res.render("maintenance.hbs");
+});*/
+
+//Public Order verfügbar machen für css, js usw.
+app.use(express.static(__dirname+"/public"));
+
+hbs.registerHelper("getCurrentYear", () => { return new Date().getFullYear();});
+hbs.registerHelper("screamIt", (text) => {return text.toUpperCase();});
+
+app.get("/", (req, res) => {
+    res.render("index.hbs", {
+        pageTitle: "Startseite",
+        welcomeMessage: "He da"
+    });
+});
+
+app.get("/about", (req,res) => {
+    res.render("about.hbs", {
+        pageTitle: "About"
+    });
+});
+
+app.get("/bad", (req,res) => {
+    res.send({
+        error: "Fehler 123"
+    });
+
+});
+
+
+app.listen(3000, () => {console.log("starte server")});
